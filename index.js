cd3457d4b6214e4fe8a4fd48762571dc3a3b8d10
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-03 21:11:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = city.value;
+}
