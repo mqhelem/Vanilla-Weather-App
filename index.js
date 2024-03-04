ad454acff6621638ade0a89ba6ee439cb5a9aa5b
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-04 09:44:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function searchCity(event) {
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = city.value;
   let apiKey = "50863a45edtc0d26o66bb25fa39f84fc";
-  let url = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}`;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}&units=metric`;
   axios.get(url).then(displayWeather);
   console.log(response.data);
 }
