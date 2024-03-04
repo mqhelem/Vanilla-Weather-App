b3911b49f77b54658b0cfcd32a5ed5eb3de57472
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-04 12:17:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function searchCity(event) {
   event.preventDefault();
   let apiKey = "50863a45edtc0d26o66bb25fa39f84fc";
   let city = document.querySelector("#city").value;
-  let h1 = document.querySelector("h1");
+  let currentCity = document.querySelector("#current-city");
   h1.innerHTML = city.value;
   let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
 `;
