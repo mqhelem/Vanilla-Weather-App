eda7d0603f8645d778da870ac5ddd0f0d547ecc2
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-04 09:35:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ function searchCity(event) {
   let apiKey = "50863a45edtc0d26o66bb25fa39f84fc";
   let url = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}`;
   axios.get(url).then(displayWeather);
+  console.log(response.data);
+}
+function displayWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.temperature.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.condition.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = response.data.wind.speed;
 }
