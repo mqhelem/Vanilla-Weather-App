9170e2f4aee15d4a09f8def3cce3c9dacd3d22c5
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-04 16:06:38 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 function searchCity(event) {
   event.preventDefault();
   let apiKey = "50863a45edtc0d26o66bb25fa39f84fc";
-  let city = document.querySelector("#city").value;
+  let city = document.querySelector("#city");
   let currentCity = document.querySelector("#current-city");
-  h1.innerHTML = city.value;
-  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+  currentCity.innerHTML = city.value;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}&units=metric
 `;
-  axios.get(url).then(searchCity);
+  axios.get(url).then(weatherData);
 }
+let searchForm = document.querySelector("#searchForm");
+searchForm.addEventListener("submit", searchCity);
 
 function weatherData(response) {
+  console.log(response.data);
   let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = response.data.temperature;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+
+  let weatherCondition = document.querySelector("#description");
+  weatherCondition.innerHTML = response.data.condition.description;
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  let weatherIcon = document.querySelector("#icon");
+  weatherIcon.src = `${response.data.condition.icon_url}`;
+
+  let date = new Date(response.data.time * 1000);
+  let dateFormat = document.querySelector("#date");
+  let formattedDate = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+  dateFormat.innerHTML = formattedDate;
+  console.log(formattedDate);
 }
