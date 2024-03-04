3f247644c3720c0c49ffac9c4aa02eb08aafad57
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-04 11:20:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 function searchCity(event) {
   event.preventDefault();
-  let city = document.querySelector("#city");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = city.value;
   let apiKey = "50863a45edtc0d26o66bb25fa39f84fc";
-  let url = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}&units=metric`;
-  axios.get(url).then(displayWeather);
-  console.log(response.data);
+  let city = document.querySelector("#city").value;
+  let h1 = document.querySelector("#h1");
+  h1.innerHTML = city.value;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+`;
+  axios.get(url).then(searchCity);
 }
-function displayWeather(response) {
+
+function weatherData(response) {
   let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(response.data.temperature);
-  let description = document.querySelector("#description");
-  description.innerHTML = response.data.temperature.description;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.condition.humidity;
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = response.data.wind.speed;
+  temperature.innerHTML = response.data.temperature;
 }
-console.log(humidity);
-console.group(wind);
